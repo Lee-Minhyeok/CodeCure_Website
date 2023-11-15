7dc9895f9df5bf718bafd9e1888f0406242b697b
Lee-Minhyeok <lmh0911@kakao.com>
2023-11-15 16:19:49 +0900

    404 not found

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import QnA from './QnA';
 import Login from './login';
 import SignUp from './signup';
 import Introduce from './introduce';
+import Notfound from './notfound';
 
 function App() {
 	return (
-		<div className='App'>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Main />}></Route>
-					<Route path="/introduce" element={<Introduce />}></Route>
-					<Route path="/announce/*" element={<Announce />}></Route>
-					<Route path="/board/*" element={<Board />}></Route>
-					<Route path="/QnA/*" element={<QnA />}></Route>
-					<Route path="/login/*" element={<Login />}></Route>
-					<Route path="/signup/*" element={<SignUp />}></Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />}></Route>
+				<Route path="/introduce" element={<Introduce />}></Route>
+				<Route path="/announce/*" element={<Announce />}></Route>
+				<Route path="/board/*" element={<Board />}></Route>
+				<Route path="/QnA/*" element={<QnA />}></Route>
+				<Route path="/login/*" element={<Login />}></Route>
+				<Route path="/signup/*" element={<SignUp />}></Route>
+				<Route path="/*" element={<Notfound />}></Route>
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './main.css';
+//import './main.css';
 import Menubar from './menubar.js';
+import styled from 'styled-components';
+
+const M = styled.div`
+	align-items: center;
+`
 
 function Main() {
 	return (
-		<div className="Board">
+		<>
 			<Menubar />
-			<div className="main">
+			<M>
 				<h3>메인페이지</h3>
 				<Link to="/announce">공지사항</Link><br></br>
 				<Link to="/board">게시판</Link><br></br>
 				<Link to="/QnA">Q&A</Link><br></br>
 				<Link to="/login">로그인</Link><br></br>
-			</div>
-		</div>
+			</M>
+		</>
 	);
 };
 
