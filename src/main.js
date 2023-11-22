0c50feccac33caf8a95f2cdb84dcc4e68516ed23
kim-guenyoung <rmsdud0527@naver.com>
2023-11-22 18:29:49 +0900

    1122 김근영

--- src/main.js
+++ src/main.js
@@ -3,23 +3,53 @@ import { Link } from 'react-router-dom';
 import { D } from './Board_style.js';
 //import './main.css';
 import Menubar from './menubar.js';
-import styled from 'styled-components';
-
-const M = styled.div`
-	align-items: center;
-`
+import {M} from './main_style.js';
+import {A} from './main_style.js';
+import {Search} from './main_style.js';
+import {Slogan} from './main_style.js';
+import {Visit} from './main_style.js';
+import {Calender} from './main_style.js';
+import {Search_btn} from './main_style.js';
+// import {Week} from './main_style.js';
 
 function Main() {
 	return (
 		<D>
 			<Menubar />
+			<Search placeholder="검색어를 입력하세요"/>
+			<Search_btn >
+				입력
+			</Search_btn>
+			<Calender>
+				<div className="app">
+					<h1>Calendar</h1>
+						{/* <div>1</div>
+						<div>2</div>
+						<div class="1st_week">3</div>
+						<div class="1st_week">4</div>
+						<div class="1st_week">5</div>
+						<div class="1st_week">6</div>
+						<div class="1st_week">7</div> */}
+					<img src="/Users/kimgeunyoung/CodeCure_Website/src/calender.png" alt = "img"></img>
+				</div>
+			</Calender>
+			<Slogan>
+				
+			</Slogan>
+			
 			<M>
-				<h3>메인페이지</h3>
-				<Link to="/announce">공지사항</Link><br></br>
-				<Link to="/board">게시판</Link><br></br>
-				<Link to="/QnA">Q&A</Link><br></br>
-				<Link to="/login">로그인</Link><br></br>
+				<div class="upcoming_box">
+					<A className="upcoming">다가올 일정</A>
+					<A className="upcoming">10/9 한글날</A>
+                    <A className="upcoming">10/23 중간고사</A>
+                    <A className="upcoming">12/25 성탄절</A>
+				</div>
 			</M>
+			<Visit>
+				<div>
+					Today 방문자 수
+				</div>
+			</Visit>
 		</D>
 	);
 };
